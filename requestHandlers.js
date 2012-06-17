edbbc00cb3ef1d8da9665187717e390ed8abf21a
S4RK <rajeshkumar_sa@yahoo.co.in>
2012-06-17 22:56:36 +0530

    dice random generator and attack strategy impl

--- requestHandlers.js
+++ requestHandlers.js
@@ -17,6 +17,14 @@ fs.readFile('first_cut.html', function(err, data){
 						});
 }
 
+function cb_new(response) {
+  console.log("Request handler 'cb' was called.");
+fs.readFile('second_cut.html', function(err, data){
+						response.writeHead(200, {'Content-Type':'text/html'});
+						response.write(data);
+						response.end();
+						});
+}
 function poke(response) {
   console.log("Request handler 'poke' was called.");
 fs.readFile('poke.html', function(err, data){
@@ -25,6 +33,17 @@ fs.readFile('poke.html', function(err, data){
 						response.end();
 						});
 }
+
+function cbnewjs(response) {
+  console.log("Request handler 'cbjs' was called.");
+fs.readFile('cb_new.js', function(err, data){
+						response.writeHead(200, {'Content-Type':'text/html'});
+						response.write(data);
+						response.end();
+						});
+}
+
+
 function cbjs(response) {
   console.log("Request handler 'cbjs' was called.");
 fs.readFile('cb.js', function(err, data){
@@ -43,6 +62,8 @@ fs.readFile('raphael.js', function(err, data){
 }
 exports.raphaeljs = raphaeljs;
 exports.cbjs = cbjs;
+exports.cbnewjs = cbnewjs;
 exports.cb = cb;
+exports.cb_new = cb_new;
 exports.notif = notif;
 exports.poke = poke;
